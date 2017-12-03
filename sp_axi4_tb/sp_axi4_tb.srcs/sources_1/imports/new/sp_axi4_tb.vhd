c97aa6ec6d31a501727ecef35d69fdeb75e1eeb9
jrcharlo <jrcharlo@buffalo.edu>
2017-12-03 01:10:31 -0500

    New setup for TB.

--- sp_axi4_tb/sp_axi4_tb.srcs/sources_1/imports/new/sp_axi4_tb.vhd
+++ sp_axi4_tb/sp_axi4_tb.srcs/sources_1/imports/new/sp_axi4_tb.vhd
@@ -43,47 +43,82 @@ entity sp_axi4_tb is
     sp_error : in std_logic; --(1 for error)
     
     sw : in std_logic_vector(3 downto 0);
-    led : out std_logic_vector(3 downto 0);
-    led0_b : out std_logic;
     led0_r : out std_logic;
     led0_g : out std_logic;
+    led0_b : out std_logic;
+    led : out std_logic_vector(3 downto 0);
     btn : in std_logic_vector(3 downto 0)
   );
 end sp_axi4_tb;
 
 architecture Behavioral of sp_axi4_tb is
 
-signal tmp : std_logic_vector(3 downto 0);
+--signal tmp : std_logic_vector(3 downto 0);
 
 begin
-    sp_sign_extend <= '0';
-    sp_op_len <= "00";
-    sp_addr <= x"80000000";
-    tmp <= sw;
-    
-    process(btn(0)) begin
-        sp_data <= x"0000000" & tmp;
-        sp_read <= '0';
-        sp_write <= '1';
-    end process;
 
-    process(sp_over, sp_error) begin
-        if(sp_error = '1') then
-            led0_r <= '1';
-            led0_g <= '0';
-            led0_b <= '0';
-            sp_write <= '0';
-        elsif(sp_over = '1') then
-            led0_r <= '0';
-            led0_g <= '1';
-            led0_b <= '0';
-            sp_write <= '0';           
-        end if;
-    end process;
-    
-    process(btn(3)) begin
-        sp_read <= '1';
-        led <= sp_data(3 downto 0);
-    end process;
+--tmp <= sw;
+led <= sp_data(3 downto 0);
+
+sp_addr <= x"80000000";
+sp_op_len <= "00";
+sp_sign_extend <= '0';
+
+process(btn(0)) begin
+    sp_read <= '0';
+    sp_data <= x"0000000" & sw;--tmp;
+    sp_write <= '1';
+end process;
+
+process(sp_over, sp_error) begin
+    if(sp_error = '1') then
+        led0_r <= '1';
+        led0_g <= '0';
+        led0_b <= '0';
+        sp_read <= '0';
+        sp_write <= '0';
+    elsif(sp_over = '1') then
+        led0_r <= '0';
+        led0_g <= '1';
+        led0_b <= '0';
+        sp_read <= '0';
+        sp_write <= '0';
+    end if;
+end process;
+
+process(btn(1)) begin
+    sp_data <= x"00000000";
+    led0_r <= '0';
+    led0_g <= '0';
+    led0_b <= '1';
+end process;
+
+process(btn(3)) begin
+    sp_write <= '0';
+    sp_read <= '1';
+end process;
+
+--for use without board I/O
+--    stimulus : process begin
+--        sp_read <= '0';
+--        sp_sign_extend <= '0';
+--        sp_op_len <= "00";
+--        sp_addr <= x"80000000";
+--        sp_data <= x"00000003";
+--        sp_write <= '1';
+        
+--        wait for 10 ns;
+        
+--        sp_write <= '0';
+--        sp_data <= x"00000000";
+        
+--        wait for 10 ns;
+        
+--        sp_read <= '1';
+        
+--        wait for 10 ns;
+        
+        
+--    end process;
 
 end Behavioral;
