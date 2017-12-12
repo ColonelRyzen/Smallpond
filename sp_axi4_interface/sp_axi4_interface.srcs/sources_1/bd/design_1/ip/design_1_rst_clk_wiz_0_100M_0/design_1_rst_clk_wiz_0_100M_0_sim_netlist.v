0fb9b5cdbed871a17fec7654eea70549715cac92
jrcharlo <jrcharlo@buffalo.edu>
2017-12-12 13:02:12 -0500

    New project to check simulation errors out.

--- sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_sim_netlist.v
+++ sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_sim_netlist.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
-// Tool Version: Vivado v.2017.3 (lin64) Build 2018833 Wed Oct  4 19:58:07 MDT 2017
-// Date        : Mon Dec 11 11:31:59 2017
-// Host        : bonner running 64-bit Ubuntu 17.10
+// Tool Version: Vivado v.2017.3.1 (lin64) Build 2035080 Fri Oct 20 14:20:00 MDT 2017
+// Date        : Tue Dec 12 11:21:33 2017
+// Host        : octopus-tetricus running 64-bit unknown
 // Command     : write_verilog -force -mode funcsim
-//               /home/lab/Documents/justin/Smallpond/sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_sim_netlist.v
+//               /home/jrcharlo/Smallpond/sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_sim_netlist.v
 // Design      : design_1_rst_clk_wiz_0_100M_0
 // Purpose     : This verilog netlist is a functional simulation representation of the design and should not be modified
 //               or synthesized. This netlist cannot be used for SDF annotated simulation.
--- sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_stub.v
+++ sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
-// Tool Version: Vivado v.2017.3 (lin64) Build 2018833 Wed Oct  4 19:58:07 MDT 2017
-// Date        : Mon Dec 11 11:31:59 2017
-// Host        : bonner running 64-bit Ubuntu 17.10
+// Tool Version: Vivado v.2017.3.1 (lin64) Build 2035080 Fri Oct 20 14:20:00 MDT 2017
+// Date        : Tue Dec 12 11:21:33 2017
+// Host        : octopus-tetricus running 64-bit unknown
 // Command     : write_verilog -force -mode synth_stub
-//               /home/lab/Documents/justin/Smallpond/sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_stub.v
+//               /home/jrcharlo/Smallpond/sp_axi4_interface/sp_axi4_interface.srcs/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_0_100M_0/design_1_rst_clk_wiz_0_100M_0_stub.v
 // Design      : design_1_rst_clk_wiz_0_100M_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7a35ticsg324-1L
