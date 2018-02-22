fc275dde848ef83e43946221b8b13097d72c30d2
Zachary Salim <zjsalim@buffalo.edu>
2018-02-22 11:20:13 -0500

    Created empty testbench file

---
+++ register_file_array/register_file_array.srcs/sim_1/new/register_file_array_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/22/2018 11:19:48 AM
+-- Design Name: 
+-- Module Name: register_file_array_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity register_file_array_tb is
+--  Port ( );
+end register_file_array_tb;
+
+architecture Behavioral of register_file_array_tb is
+
+begin
+
+
+end Behavioral;
