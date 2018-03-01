2a45b0a2f64311bda1d044895a2413d0594c71c0
Zachary Salim <zjsalim@buffalo.edu>
2018-03-01 10:58:08 -0500

    Added top file, register file, control unit, and alu.

---
+++ smallpond_top/smallpond_top.srcs/sources_1/new/smallpond_top.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/01/2018 10:46:48 AM
+-- Design Name: 
+-- Module Name: smallpond_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity smallpond_top is
+--  Port ( );
+end smallpond_top;
+
+architecture Behavioral of smallpond_top is
+
+begin
+
+
+end Behavioral;
