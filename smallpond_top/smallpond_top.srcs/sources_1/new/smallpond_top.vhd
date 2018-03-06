6670f7b2db143f27837ab3b74612c77f49aaf760
Zachary Salim <zjsalim@buffalo.edu>
2018-03-06 18:57:58 -0500

    Correct project structure. top file has main clock process setting the clock to 2 MHz.

--- smallpond_top/smallpond_top.srcs/sources_1/new/smallpond_top.vhd
+++ smallpond_top/smallpond_top.srcs/sources_1/new/smallpond_top.vhd
@@ -39,6 +39,31 @@ end smallpond_top;
 architecture Behavioral of smallpond_top is
 
 signal cpu_clk : STD_LOGIC := '0';
+----CONTROL UNIT SIGNALS
+--signal ctrl_reg_write_out : STD_LOGIC := '0';
+--signal ctrl_counter_bit_out : STD_LOGIC := '0';
+--signal ctrl_cpsr_set_bit_out : STD_LOGIC := '0';
+--signal ctrl_alu_src_out : STD_LOGIC := '0';
+--signal ctrl_sub_out : STD_LOGIC := '0';
+--signal ctrl_pc_write_out : STD_LOGIC := '0';
+--signal ctrl_src_out : STD_LOGIC := '0';
+--signal ctrl_jum_out : STD_LOGIC := '0';
+--signal ctrl_mem_read_out : STD_LOGIC := '0';
+--signal ctrl_mem_write_out : STD_LOGIC := '0';
+--signal ctrl_mem_to_reg_out : STD_LOGIC := '0';
+
+----REGISTER FILE SIGNALS
+--signal reg_pc_data_out : STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+--signal reg_cpsr_cond_bits_control_out : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+--signal 
+
+----ALU SIGNALS
+--signal alu_result_out : STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+--signal alu_cpsr_out : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+--signal alu_a_input : STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+--signal alu_b_input : STD_LOGIC_VECTOR(31 downto 0) := x"00000000";
+--signal alu_op_in : STD_LOGIC_VECTOR(3 downto 0) := x"00000000";
+--signal alu_sub : STD_LOGIC := '0';
 
 
 begin
@@ -48,12 +73,13 @@ begin
         if rising_edge(clk_in) then
             if clk_counter = 500 then
                 clk_counter := 0;
-                cpu_clk <= NOT cpu_clk;
+                cpu_clk <= not cpu_clk;
             else
                 clk_counter := clk_counter + 1;
             end if;
         end if;
     end process;
+    
     
     
 
